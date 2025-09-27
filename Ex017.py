2d1a5514a4a48640ff7ee48de27135d3bd40e8e2
Victor Cabral <119380591+vectorcabral@users.noreply.github.com>
2025-09-27 16:16:34 -0300

    Add files via upload

---
+++ Ex017.py
@@ -0,0 +1,14 @@
+'''
+________________________________________________________________________________________________________________________________________________
+________________________________________________________________________________________________________________________________________________
+
+Faça um programa que leia o comprimento do Cateto Oposto e do Cateto Adjacente de um triângulo e mostre o comprimento
+da hipotenusa.
+________________________________________________________________________________________________________________________________________________
+________________________________________________________________________________________________________________________________________________
+'''
+from math import sqrt
+co = float(input('Digite um comprimento: '))
+ca = float(input('Digite outro comprimento: '))
+hip = sqrt(co**2+ca**2)
+print('O Triângulo cujo o Cateto Oposto é {:.2f} e o Cateto Adjacente é {:.2f}, tem sua hipotenusa no valor de {:.1f}'.format(co,ca,hip))
